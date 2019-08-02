80e8a6af4797ef540430d4249ac02fd3305f8420
MaeenBadea <34209166+MaeenBadea@users.noreply.github.com>
2019-08-02 12:49:14 +0400

    Updates

---
+++ precache-manifest.d0be7fb0d30e47faf1186fd2cfdd0ba4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ab14946bc3537ecf3c51f2ff713af2",
+    "url": "/personal-site/index.html"
+  },
+  {
+    "revision": "a254371fc4115f5f26ce",
+    "url": "/personal-site/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "c90458145815e5347a82",
+    "url": "/personal-site/static/css/main.f9f75f66.chunk.css"
+  },
+  {
+    "revision": "a254371fc4115f5f26ce",
+    "url": "/personal-site/static/js/2.fe392ec9.chunk.js"
+  },
+  {
+    "revision": "c90458145815e5347a82",
+    "url": "/personal-site/static/js/main.fb221687.chunk.js"
+  },
+  {
+    "revision": "d33cc63b615d83e32684",
+    "url": "/personal-site/static/js/runtime~main.039145d2.js"
+  },
+  {
+    "revision": "52bec8f6a5fb989a25429cd17b52c036",
+    "url": "/personal-site/static/media/about-us.52bec8f6.png"
+  },
+  {
+    "revision": "50953027678f2e493ea34b63733f26cd",
+    "url": "/personal-site/static/media/home-right.50953027.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
